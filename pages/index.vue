03cc7344cbb23abf91457c9f78dc0f9ded78905d
Mehmet Emin Eker <Mehmet.Eker@d-teknoloji.com.tr>
2024-05-14 15:58:09 +0300

    nuxt app created

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Plus } from 'lucide-vue-next'
+
+</script>
+
+<template>
+  <div class="container mx-auto pt-12">
+    <div class="flex justify-between mb-4">
+      <Button>
+        <Plus class="w-4 h-4 mr-2" /> Create Room
+      </Button>
+    </div>
+  </div>
+</template>
