0ad4f68793e3b191c29309be2ebfdf467ed6b309
Mehmet Emin Eker <Mehmet.Eker@d-teknoloji.com.tr>
2024-05-18 19:33:50 +0300

    tasarım sadeleştirildi

--- pages/[roomId].vue
+++ pages/[roomId].vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
-import { ScrollArea } from '@/components/ui/scroll-area'
 import { Plus, Smile, Check, ChevronsUpDown } from 'lucide-vue-next'
-import { Textarea } from '@/components/ui/textarea'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 import {
@@ -15,119 +12,65 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 
-import { ref } from 'vue'
-
-import { cn } from '@/lib/utils'
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from '@/components/ui/command'
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover'
-
-const pokerTypes = [
-  { value: 'fibonacci', label: 'Fibonacci' },
-  { value: 't-shirt-size', label: 'T-Shirt Size' },
-  { value: 'modified-fibonacci', label: 'Modified Fibonacci' },
-]
-
-const open = ref(false)
-const value = ref('fibonacci')
+const sofiaRole = ref('Owner')
+const jacksonRole = ref('Member')
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-4">
-    <div class="col-span-3">
-      <Textarea />
-    </div>
-    <div class="">
-      <Popover v-model:open="open" class="w-full">
-        <PopoverTrigger as-child>
-          <Button variant="outline" role="combobox" :aria-expanded="open" class="w-full justify-between">
-            {{ value
-              ? pokerTypes.find((pokerType) => pokerType.value === value)?.label
-              : "Select Poker Type..." }}
-            <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent class="w-full p-0">
-          <Command>
-            <CommandList>
-              <CommandGroup>
-                <CommandItem v-for="pokerType in pokerTypes" :key="pokerType.value" :value="pokerType.value" @select="(ev) => {
-                  if (typeof ev.detail.value === 'string') {
-                    value = ev.detail.value
-                  }
-                  open = false
-                }">
-                  {{ pokerType.label }}
-                  <Check :class="cn(
-                    'ml-auto h-4 w-4',
-                    value === pokerType.value ? 'opacity-100' : 'opacity-0',
-                  )" />
-                </CommandItem>
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
-    </div>
-    <div class="col-span-3">
-
-    </div>
-    <div class="">
-
-      <Card>
-        <CardContent class="pt-6">
-          <div class="flex flex-col gap-4">
-            <div class="flex items-center gap-2">
-              <Avatar>
-                <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-              <div class="flex-1">
-                Mehmet
-              </div>
-              <div>
-                <Smile class="h-4 w-4 text-green-500" />
-              </div>
-            </div>
-            <div class="flex items-center gap-2">
-              <Avatar>
-                <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-              <div class="flex-1">
-                Mehmet
-              </div>
-              <div>
-                <Smile class="h-4 w-4 text-green-500" />
-              </div>
-            </div>
-            <div class="flex items-center gap-2">
-              <Avatar>
-                <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-              <div class="flex-1">
-                Mehmet
-              </div>
-              <div>
-                <Smile class="h-4 w-4 text-green-500" />
-              </div>
-            </div>
+  <Card>
+    <CardHeader class="grid grid-cols-[minmax(0,1fr)_110px] items-start gap-4 space-y-0">
+      <div class="">
+        <CardTitle>Developers</CardTitle>
+        <CardDescription>
+          Invite your team members.
+        </CardDescription>
+      </div>
+      <div class="text-right">
+        <Button variant="secondary">Invite</Button>
+      </div>
+    </CardHeader>
+    <CardContent class="grid gap-6">
+      <div class="flex items-center justify-between space-x-4">
+        <div class="flex items-center space-x-4">
+          <Avatar>
+            <AvatarImage src="/avatars/01.png" />
+            <AvatarFallback>OM</AvatarFallback>
+          </Avatar>
+          <div>
+            <p class="text-sm font-medium leading-none">
+              Sofia Davis
+            </p>
+            <p class="text-sm text-muted-foreground">
+              m@example.com
+            </p>
           </div>
-        </CardContent>
-      </Card>
+        </div>
 
-    </div>
+      </div>
+      <div class="flex items-center justify-between space-x-4">
+        <div class="flex items-center space-x-4">
+          <Avatar>
+            <AvatarImage src="/avatars/02.png" />
+            <AvatarFallback>JL</AvatarFallback>
+          </Avatar>
+          <div>
+            <p class="text-sm font-medium leading-none">
+              Jackson Lee
+            </p>
+            <p class="text-sm text-muted-foreground">
+              p@example.com
+            </p>
+          </div>
+        </div>
 
-  </div>
+      </div>
+    </CardContent>
+  </Card>
+
+  <Button>
+    Start
+  </Button>
+  <Button>
+    Open
+  </Button>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,42 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
 import { Plus } from 'lucide-vue-next'
+import { Label } from '@/components/ui/label'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 </script>
 
 <template>
-  <div class="flex justify-center">
-    <Button>
-      <Plus class="w-4 h-4 mr-2" /> Create Room
-    </Button>
+  <div class="">
+    <h3 class="mb-2">Select a poker type</h3>
+    <RadioGroup default-value="fibonacci" class="grid grid-cols-3 gap-4 ">
+      <div>
+        <RadioGroupItem id="fibonacci" value="fibonacci" class="peer sr-only" />
+        <Label for="fibonacci"
+          class="flex flex-col items-center justify-between cursor-pointer rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary">
+          <img class="h-28 w-28 mb-2" src="~/assets/icons/fibonacci.svg" />
+          Fibonacci
+        </Label>
+      </div>
+      <div>
+        <RadioGroupItem id="modified-fibonacci" value="modified-fibonacci" class="peer sr-only" />
+        <Label for="modified-fibonacci"
+          class="flex flex-col items-center justify-between cursor-pointer rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary">
+          <img class="h-28 w-28 mb-2" src="~/assets/icons/modified-fibonacci.svg" />
+          Modified Fibonacci
+        </Label>
+      </div>
+      <div>
+        <RadioGroupItem id="t-shirt-sizing" value="t-shirt-sizing" class="peer sr-only" />
+        <Label for="t-shirt-sizing"
+          class="flex flex-col items-center justify-between cursor-pointer rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary">
+          <img class="h-28 w-28 mb-2" src="~/assets/icons/t-shirt-sizing.svg" />
+          T-Shirt Sizing
+        </Label>
+      </div>
+    </RadioGroup>
   </div>
+
+  <Button>
+    <Plus class="w-4 h-4 mr-2" /> Create Room
+  </Button>
 </template>
